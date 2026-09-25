ce15fe7450a804db2fc36ac19cd7bb7a75c9a89a
pve/apps <axelroman.dev@outlook.com>
2026-09-25 02:45:35 +0000

    Se edito los template de nginx
    Se editaron los archivos para evitar que nginx se caiga sino no puede
    resolverse

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 set -e
 
+# Borra las configs generadas antes para no dejar restos de templates eliminados
+for template in /etc/nginx/templates/*.template; do :; done
+rm -f /etc/nginx/conf.d/*.conf
+
 for template in /etc/nginx/templates/*.template; do
+  [ -e "$template" ] || continue
   filename=$(basename "$template" .template)
   envsubst '${DOMAIN}' < "$template" > "/etc/nginx/conf.d/$filename"
 done
 
+nginx -t
 exec nginx -g 'daemon off;'
